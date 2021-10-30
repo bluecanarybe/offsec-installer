a3d74a71bf1c9b392438803e9118c63e7705d8a4
dennis@bluecanary.be <dennis@bluecanary.be>
2021-10-30 11:15:53 +0200

    add cleanup

--- install.sh
+++ install.sh
@@ -32,6 +32,7 @@ wget https://golang.org/dl/go1.17.2.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.17.2.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 apt-get -y install python3-pip
+rm go1.17.2.linux-amd64.tar.gz
 
 cecho "Installing helpesr and QoL stuff"
 go get -u github.com/tomnomnom/gron
