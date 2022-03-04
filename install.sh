5c7b1e785957bf935e134b44697ffb18bdf8727c
dennis@bluecanary.be <dennis@bluecanary.be>
2022-03-04 12:11:54 +0100

    unattended zsh

--- install.sh
+++ install.sh
@@ -44,7 +44,7 @@ git clone https://github.com/Zarcolio/grepaddr && cd grepaddr && bash install.sh
 
 cecho "Installing zsh"
 apt-get -y install zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 sed -i 's/robbyrussell/agnoster/' ~/.zshrc
